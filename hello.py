6820eff6836c3a472a9f56e827dae29de5199244
jss3af <jss3af@virginia.edu>
2015-02-16 16:22:54 -0500

    Added the second helper and updated hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 __author__ = 'jss3af'
 
 from helper import greeting
+from second_helper import greeting2
 
 if __name__== "__main__":
     greeting("hello")
+    greeting2("sup")
---
+++ second_helper.py
@@ -0,0 +1,4 @@
+__author__ = "jss3af"
+
+def greeting2(msg):
+    print("This is an unhelpful function. Instead of printing the msg, it just prints this.")
